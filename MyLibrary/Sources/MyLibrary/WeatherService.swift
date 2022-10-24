f7459ae9fa3127f51efcfd061b00c9bae4c707d1
jingershi <jingers@oregonstate.edu>
2022-10-23 20:37:09 -0700

    fixed mock server issues

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -10,7 +10,7 @@ enum BaseUrl :String {
 }
 
 class WeatherServiceImpl: WeatherService {
-    let url = "https://api.openweathermap.org/data/2.5/weather?lat=44.5646&lon=-123.2620&appid=0ca5e721078f1ed3c0a771d6f9c54274"
+    let url = "http://0.0.0.0:8080/data/2.5/weather"
 
     func getTemperature() async throws -> Int {
         return try await withCheckedThrowingContinuation { continuation in
